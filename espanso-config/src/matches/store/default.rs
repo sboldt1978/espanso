cd9fafea24c2e0eafa7326792b901231d5823ea4
Stephan Boldt <github@sboldt.de>
2026-01-25 18:47:28 +0100

    Update config mocks for open file menu settings

--- espanso-config/src/matches/store/default.rs
+++ espanso-config/src/matches/store/default.rs
@@ -338,6 +338,15 @@ mod tests {
         fn secure_input_notification(&self) -> bool {
             true
         }
+        fn open_file_menu_recent_files_count(&self) -> usize {
+            10
+        }
+        fn open_file_menu_recent_files_per_scope_count(&self) -> usize {
+            10
+        }
+        fn open_file_menu_yaml_editor_path(&self) -> Option<String> {
+            None
+        }
         fn stats_enabled(&self) -> bool {
             true
         }
