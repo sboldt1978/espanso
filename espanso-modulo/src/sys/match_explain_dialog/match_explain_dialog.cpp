d9ca093bf5d03c1dc758e2023df3411dd60c5fd0
Stephan Boldt <github@sboldt.de>
2026-01-26 19:53:50 +0100

    Revert "feat(match-explain): render inline file link in output"
    This reverts commit b4ae300cb8537af1f51f299366df92fc0e62086d.

--- espanso-modulo/src/sys/match_explain_dialog/match_explain_dialog.cpp
+++ espanso-modulo/src/sys/match_explain_dialog/match_explain_dialog.cpp
@@ -23,7 +23,7 @@
 #include "../interop/interop.h"
 
 #include <wx/clipbrd.h>
-#include <wx/richtext/richtextctrl.h>
+#include <wx/hyperlink.h>
 #include <wx/utils.h>
 
 #ifdef __WXMSW__
@@ -51,7 +51,9 @@ class MatchExplainDialogFrame : public wxFrame {
     wxTextCtrl *trigger_input = nullptr;
     wxButton *check_button = nullptr;
     wxButton *check_all_button = nullptr;
-    wxRichTextCtrl *output_box = nullptr;
+    wxTextCtrl *output_box = nullptr;
+    wxStaticText *file_label = nullptr;
+    wxHyperlinkCtrl *file_link = nullptr;
     wxCheckBox *json_checkbox = nullptr;
     wxButton *copy_button = nullptr;
     wxButton *close_button = nullptr;
@@ -63,12 +65,12 @@ class MatchExplainDialogFrame : public wxFrame {
     void OnActivate(wxActivateEvent &event);
     void OnClose(wxCloseEvent &event);
     void OnChar(wxKeyEvent &event);
-    void OnTextUrl(wxTextUrlEvent &event);
+    void OnFileLink(wxHyperlinkEvent &event);
 
     void NotifyFocusGained();
     void NotifyFocusLost();
     void RunCheck(bool show_all);
-    void UpdateOutput(const wxString &output, bool json_output);
+    void UpdateFileLink(const wxString &output, bool json_output);
 };
 
 MatchExplainDialogFrame::MatchExplainDialogFrame()
@@ -95,15 +97,20 @@ MatchExplainDialogFrame::MatchExplainDialogFrame()
 
     main_sizer->Add(top_sizer, 0, wxEXPAND | wxALL, 10);
 
-    output_box = new wxRichTextCtrl(panel, wxID_ANY, "", wxDefaultPosition,
-                                    wxDefaultSize,
-                                    wxTE_MULTILINE | wxTE_READONLY);
+    output_box = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition,
+                                wxDefaultSize,
+                                wxTE_MULTILINE | wxTE_READONLY | wxTE_RICH2);
     wxFont output_font = output_box->GetFont();
     output_font.SetFamily(wxFONTFAMILY_TELETYPE);
-    wxTextAttr output_attr;
-    output_attr.SetFont(output_font);
-    output_box->SetDefaultStyle(output_attr);
+    output_box->SetFont(output_font);
     main_sizer->Add(output_box, 1, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 10);
+
+    wxBoxSizer *file_sizer = new wxBoxSizer(wxHORIZONTAL);
+    file_label = new wxStaticText(panel, wxID_ANY, "Defined in:");
+    file_sizer->Add(file_label, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 6);
+    file_link = new wxHyperlinkCtrl(panel, wxID_ANY, " ", "about:blank");
+    file_sizer->Add(file_link, 1, wxALIGN_CENTER_VERTICAL);
+    main_sizer->Add(file_sizer, 0, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 10);
 
     wxBoxSizer *bottom_sizer = new wxBoxSizer(wxHORIZONTAL);
     json_checkbox = new wxCheckBox(panel, wxID_ANY, "JSON output");
@@ -123,12 +130,14 @@ MatchExplainDialogFrame::MatchExplainDialogFrame()
                        this);
     trigger_input->Bind(wxEVT_TEXT_ENTER,
                         &MatchExplainDialogFrame::OnCheck, this);
-    output_box->Bind(wxEVT_TEXT_URL, &MatchExplainDialogFrame::OnTextUrl, this);
+    file_link->Bind(wxEVT_HYPERLINK, &MatchExplainDialogFrame::OnFileLink, this);
 
     Bind(wxEVT_ACTIVATE, &MatchExplainDialogFrame::OnActivate, this);
     Bind(wxEVT_CLOSE_WINDOW, &MatchExplainDialogFrame::OnClose, this);
     Bind(wxEVT_CHAR_HOOK, &MatchExplainDialogFrame::OnChar, this);
 
+    file_label->Hide();
+    file_link->Hide();
     Centre();
 }
 
@@ -189,9 +198,11 @@ void MatchExplainDialogFrame::RunCheck(bool show_all) {
                                          json_checkbox->IsChecked() ? 1 : 0);
     if (response) {
         wxString output = wxString::FromUTF8(response);
-        UpdateOutput(output, json_checkbox->IsChecked());
+        output_box->SetValue(output);
+        UpdateFileLink(output, json_checkbox->IsChecked());
     } else {
-        UpdateOutput("", json_checkbox->IsChecked());
+        output_box->SetValue("");
+        UpdateFileLink("", json_checkbox->IsChecked());
     }
 }
 
@@ -203,20 +214,15 @@ void MatchExplainDialogFrame::OnCopy(wxCommandEvent &event) {
     }
 }
 
-void MatchExplainDialogFrame::OnTextUrl(wxTextUrlEvent &event) {
-    const long start = event.GetURLStart();
-    const long end = event.GetURLEnd();
-    wxString path;
-    if (start >= 0 && end > start) {
-        path = output_box->GetValue().Mid(start, end - start);
-    }
+void MatchExplainDialogFrame::OnFileLink(wxHyperlinkEvent &event) {
+    wxString path = file_link->GetURL();
     if (!path.IsEmpty()) {
         wxLaunchDefaultApplication(path);
     }
 }
 
-void MatchExplainDialogFrame::UpdateOutput(const wxString &output,
-                                           bool json_output) {
+void MatchExplainDialogFrame::UpdateFileLink(const wxString &output,
+                                             bool json_output) {
     wxString source_path;
 
     if (json_output) {
@@ -244,31 +250,17 @@ void MatchExplainDialogFrame::UpdateOutput(const wxString &output,
         }
     }
 
-    output_box->Freeze();
-    output_box->Clear();
-
-    if (source_path.IsEmpty()) {
-        output_box->WriteText(output);
-        output_box->Thaw();
-        return;
+    if (!source_path.IsEmpty()) {
+        file_link->SetLabel(source_path);
+        file_link->SetURL(source_path);
+        file_label->Show();
+        file_link->Show();
+    } else {
+        file_label->Hide();
+        file_link->Hide();
     }
 
-    int link_pos = output.Find(source_path);
-    if (link_pos == wxNOT_FOUND) {
-        output_box->WriteText(output);
-        output_box->Thaw();
-        return;
-    }
-
-    wxString before = output.Left(link_pos);
-    wxString after = output.Mid(link_pos + source_path.Length());
-
-    output_box->WriteText(before);
-    output_box->BeginURL(source_path);
-    output_box->WriteText(source_path);
-    output_box->EndURL();
-    output_box->WriteText(after);
-    output_box->Thaw();
+    Layout();
 }
 
 bool MatchExplainDialogApp::OnInit() {
